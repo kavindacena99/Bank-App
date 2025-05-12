412b80c5d272efc605bf3469c2dc791eb686b2db
kavinda <kavindach4@gmail.com>
2025-05-12 12:00:12 +0530

    INItial setup OK

--- App.tsx
+++ App.tsx
@@ -1,20 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, Text } from 'react-native';
+import './global.css';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+function App(){
+  return(
+    <View>
+      <Text>hi Kavinda</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ babel.config.js
@@ -0,0 +1,12 @@
+import plugin from "tailwindcss";
+
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: [
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+      "nativewind/babel",
+    ],
+    plugins:["expo-router/babel","nativewind/babel"]
+  };
+};
